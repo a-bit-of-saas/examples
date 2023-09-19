e08db850894895a2271fa3aaec75e114a6418c94
Ethan Mick <ethan@ethanmick.com>
2023-09-19 14:00:24 -0400

    feat: Works locally

---
+++ next/oauth-google/lib/session.ts
@@ -0,0 +1,16 @@
+import { User, getServerSession } from 'next-auth'
+
+export const session = async ({ session, token }: any) => {
+  session.user.id = token.id
+  return session
+}
+
+export const getUserSession = async (): Promise<User> => {
+  const authUserSession = await getServerSession({
+    callbacks: {
+      session,
+    },
+  })
+  // if (!authUserSession) throw new Error('unauthorized')
+  return authUserSession?.user
+}
